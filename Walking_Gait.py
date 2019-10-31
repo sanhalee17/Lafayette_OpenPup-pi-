c3b78d33e0e3b6f437883714048e8ef9728cb64d
Ryan Fainor <fainorr@lafayette.edu>
2019-10-31 10:18:18 -0400

    Update Walking_Gait.py

--- Walking_Gait.py
+++ Walking_Gait.py
@@ -23,18 +23,18 @@ ls = 5.00 # spine, inches
 # establish gait parameters
 
 gait_duration = 2 # seconds
-leg_pace = 100 # pace of gait
+leg_pace = 250 # pace of gait
 
-x_center = -2         #center of gravity
-x_stride_left = 0.5
-x_stride_right = -0.5
+x_center = 0.3         #center of gravity
+x_stride_left = -1.5  
+x_stride_right = -1.5  
 
 z_center = -4
-z_lift = 0.7
+z_lift = 1.5
 
 leg1_offset = 0		# front left
-leg2_offset = 0	# front right
-leg3_offset = 0		# back left
+leg2_offset = pi	# front right
+leg3_offset = pi		# back left
 leg4_offset = 0 		# back right
 
 
@@ -113,7 +113,7 @@ def set_servo_pulse(channel, pulse):
 	pulse *= 1000
 	pulse //= pulse_length
 	pwm.set_pwm(channel, 0, pulse)
-	pwm.set_pwm_freq(100)
+	pwm.set_pwm_freq(60)
 
 #calculating 2D IK angles
 
@@ -122,9 +122,9 @@ for i in range(0,len(t)):
 	
 	x1[i] = x_center + x_stride_left*sin(leg_pace*t[i]  - leg1_offset)
 	if zf1[i]>=0:
-	   z1[i] = z_center + (z_lift)*sin(leg_pace*t[i] - leg1_offset-pi/2)
+	   z1[i] =  z_center + (z_lift)*sin(leg_pace*t[i] - leg1_offset-pi/2)
 	else:
-	   z1[i] = z_center
+	   z1[i] =   z_center
 	#z1[i] = z_center + z_lift*sin(leg_pace*t[i] - leg1_offset)
 	#print(x1[i])
 
@@ -153,14 +153,14 @@ for i in range(0,len(t)):
 
 #tibia and femur angles in radians
 	angf1[i], angt1[i] = getServoAng(x1[i], z1[i], lf, lt)
-	qangf2[i], qangt2[i] = getServoAng(x2[i], z2[i], lf, lt) 
+	angf2[i], angt2[i] = getServoAng(x2[i], z2[i], lf, lt) 
 	angf3[i], angt3[i] = getServoAng(x3[i], z3[i], lf, lt) 
-	qangf4[i], qangt4[i] = getServoAng(x4[i], z4[i], lf, lt)
+	angf4[i], angt4[i] = getServoAng(x4[i], z4[i], lf, lt)
 
-	angf2[i] = pi - qangf2[i]
-	angt2[i] = pi - qangt2[i]
-	angf4[i] = pi - qangf4[i]
-	angt4[i] = pi - qangt4[i]
+	# angf2[i] = pi - qangf2[i]
+	# angt2[i] = pi - qangt2[i]
+	# angf4[i] = pi - qangf4[i]
+	# angt4[i] = pi - qangt4[i]
 
 
 	print(str(angt1[i])+", "+ str(angf1[i])+", "+str(angt2[i])+", "+str(angf2[i])+", "+str(angt3[i])+", "+str(angf3[i])+", "+str(angt4[i])+", "+str(angf4[i]))
@@ -184,46 +184,47 @@ for i in range(0,len(t)):
 
 
 #angle offset values
-tib1_offset = 250 + sangt1[0]
-fem1_offset = 610 - sangf1[0]
+tib1_offset = 900
+fem1_offset = 1050
 
-tib3_offset = 250 + sangt3[0]
-fem3_offset = 610 - sangf3[0]
+tib2_offset = 300
+fem2_offset = 170
+
+tib3_offset = 900
+fem3_offset = 1050
+
+tib4_offset = 300
+fem4_offset = 170
 
 #sending the servo angles to indivial servos 
 
 i=0
+
 while True:
-	pwm.set_pwm(0, 0, int(sangt2[i%len(sangt2)])) #port zero : right front tibia
-	i=i+1
 	
-	pwm.set_pwm(1, 0, int(sangf2[i%len(sangf2)])) #port 1: right front femur
+	pwm.set_pwm(0, 0, tib2_offset+int(sangt2[i%len(sangt2)])) #port zero : right front tibia
+	i=i+1
+	i_c=i%len(sangt1)
+
+	
+	pwm.set_pwm(1, 0, fem2_offset+int(sangf2[i_c])) #port 1: right front femur
 	pwm.set_pwm(2, 0, 500)                             #port 2: right hip
 
 
-	pwm.set_pwm(3, 0, tib1_offset+(int(sangt1[i%len(sangt1)]))) 
-	pwm.set_pwm(4, 0, fem1_offset+(int(sangf1[i%len(sangf1)])) 
-	
+	pwm.set_pwm(3, 0, tib1_offset-int(sangt1[i_c])) 
+	pwm.set_pwm(4, 0, fem1_offset-int(sangf1[i_c])) 
+	time.sleep(0.01)
+		
 	pwm.set_pwm(5, 0, 500)                            #port 5: left hip
 
-	pwm.set_pwm(8, 0, fem3_offset + int(sangf3[i%len(sangf3)])) #port 8: left back femur
-	pwm.set_pwm(9, 0, tib3_offset + int(sangt3[i%len(sangt3)])) #port 9: left back tibia
+	pwm.set_pwm(8, 0, fem3_offset - int(sangf3[i_c])) #port 8: left back femur
+	pwm.set_pwm(9, 0, tib3_offset - int(sangt3[i_c])) #port 9: left back tibia
 	pwm.set_pwm(10, 0, 500)                            #port 10: left back hip
 	
-	pwm.set_pwm(12, 0, (-742+610)+int(sangf4[i%len(sangf4)])) #port 6: right back femur  
-	pwm.set_pwm(7, 0, (610-584)+int(sangt4[i%len(sangt4)])) #port 7: right back tibia
+	pwm.set_pwm(12, 0, fem4_offset+int(sangf4[i%len(sangf4)])) #port 6: right back femur  
+	pwm.set_pwm(7, 0, tib4_offset+int(sangt4[i%len(sangt4)])) #port 7: right back tibia
 	pwm.set_pwm(13, 0, 500)                           #port 11: right back hip                            #port 11: right back hip 
-	#print(str(time.time())+", "+str((610-315)+int(sangt1[i%len(sangt1)]))+", "+ str((610-157) +int(sangf1[i%len(sangf1)]))+", "+	str((610-461)+int(sangt2[i%len(sangt2)])) +", "+ str((-813+610)+int(sangf2[i%len(sangf2)])) +", "+ str((610-438)+int(sangt3[i%len(sangt3)]))+", "+ str((610-86)+int(sangf3[i%len(sangf3)]))+", "+ str((610-584)+int(sangt4[i%len(sangt4)]))+", "+ str((-742+610)+int(sangf4[i%len(sangf4)])))
-
-
-
-
+	print(str(time.time())+", "+str(int(sangt1[i%len(sangt1)]))+", "+ str(int(sangf1[i%len(sangf1)]))+", "+	str(int(sangt2[i%len(sangt2)])) +", "+ str(int(sangf2[i%len(sangf2)])) +", "+ str(int(sangt3[i%len(sangt3)]))+", "+ str(int(sangf3[i%len(sangf3)]))+", "+ str(int(sangt4[i%len(sangt4)]))+", "+ str(int(sangf4[i%len(sangf4)])))
 
 
 	
-
-
-
-
-
-
